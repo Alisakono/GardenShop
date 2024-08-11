4441a6cec5e48e836968e3c3e02db14d86a36e26
Alisakono <alisa.konovalov@web.de>
2024-08-11 15:49:46 +0200

    add ProductController

--- src/main/java/com/telran/controller/ProductController.java
+++ src/main/java/com/telran/controller/ProductController.java
@@ -2,18 +2,18 @@ package com.telran.controller;
 
 import com.telran.entity.Product;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import com.telran.service.ProductService;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @RestController
 @RequestMapping("/garden")
 public class ProductController {
-    private List<Product> products;
+
     private final ProductService service;
 
     @Autowired
@@ -27,13 +27,43 @@ public class ProductController {
     }
 
     @GetMapping("/all")
-    public List<Product> getAllProducts() {
+    public List<Product> getAll() {
 
         return service.getAll();
     }
+
     @GetMapping("/searchByName")
     public List<Product> getAllByName(@RequestParam String name) {
 
         return service.getAllByName(name);
+    }
+
+    @PostMapping
+    public ResponseEntity<Product> addProduct(@RequestBody Product product) {
+        if (product.getName() == null || product.getName().isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        if (product.getPrice() == null || product.getPrice().compareTo(BigDecimal.ZERO) <= 0) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        service.add(product);
+        return new ResponseEntity<>(product, HttpStatus.CREATED);
+    }
+
+    @PutMapping
+    public ResponseEntity<Product> updateProduct(@RequestBody Product product) {
+        boolean isUpdated = service.updateProduct(product);
+        if (isUpdated) {
+            return new ResponseEntity<>(product, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(product, HttpStatus.NO_CONTENT);
+        }
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Product> deleteProduct(@RequestBody Product product) {
+        service.remove(product);
+        return new ResponseEntity<>(product, HttpStatus.ACCEPTED);
     }
 }
