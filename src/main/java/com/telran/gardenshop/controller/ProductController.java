08623742dbef00116063a0da8be21fc66b22dc56
Alisakono <alisa.konovalov@web.de>
2024-09-17 14:45:44 +0200

    ProductMapper fix

--- src/main/java/com/telran/gardenshop/controller/ProductController.java
+++ src/main/java/com/telran/gardenshop/controller/ProductController.java
@@ -43,6 +43,7 @@ public class ProductController {
         Page<ProductResponseDto> productsByFilters = service.getProductsByFilters(
                pageable,category, minPrice, maxPrice, discount);
         return ResponseEntity.ok(productsByFilters);
+        
     }
 
     @PostMapping("")
