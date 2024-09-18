75e950cf890cc187a37597ac73a44f47037c17ba
Eugen Konovalov <149701143+EugenXkonovalov@users.noreply.github.com>
2024-09-18 21:11:01 +0200

    Add Cart

--- src/main/java/com/telran/gardenshop/exceptionhandler/ProductNotFoundException.java
+++ src/main/java/com/telran/gardenshop/exceptionhandler/ProductNotFoundException.java
@@ -1,7 +1,11 @@
 package com.telran.gardenshop.exceptionhandler;
 
-public class ProductNotFoundException extends RuntimeException{
+public class ProductNotFoundException extends RuntimeException {
     public ProductNotFoundException(Long id) {
         super("Product with id " + id + " not found");
+    }
 
-    }}
+    public ProductNotFoundException(String id) {
+        super("Product with id " + id + " not found");
+    }
+}
